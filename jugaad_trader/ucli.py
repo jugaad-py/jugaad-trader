ca011d174268716488c7ff7c91314903f6470c7f
My name is Anthony Gonsalves <jugaad@trader>
2020-09-11 14:23:23 +0000

    :tada: Upstox cli updated

--- jugaad_trader/ucli.py
+++ jugaad_trader/ucli.py
@@ -18,7 +18,7 @@ def upstox():
     """ Command line utilities for managing Upstox account
         Get started by creating a session
 
-        $ jtrader zerodha startsession
+        $ jtrader upstox startsession
     """
     pass
 
@@ -37,4 +37,32 @@ def savecreds():
         config.write(fp)
         click.echo(click.style("Saved credentials successfully", fg='green'))
 
+@upstox.command()
+def configdir():
+    """Print app config directory location"""
+    click.echo(app_dir)
 
+
+@upstox.command()
+@click.argument("config")
+def rm(config):
+    """Delete stored credentials or sessions config
+
+        $ jtrader upstox rm CREDENTIALS
+    """
+    if config == "CREDENTIALS":
+        try:
+            os.remove(os.path.join(app_dir, cred_file))
+        except FileNotFoundError:
+            click.echo("Could not find any credentials, you can save it again using 'jtrader upstox savecreds'")
+        else:
+            click.echo("Successfully deleted credentials config, you can save it again using 'jtrader upstox savecreds`")
+    
+    if config == "SESSION":
+        try:
+            os.remove(os.path.join(app_dir, session_file))
+        except FileNotFoundError:
+            click.echo("Could not find the session config, you can save it again using 'jtrader upstox startsession'")
+        else:
+            click.echo("Successfully deleted, you can save it again using 'jtrader upstox startsession'")
+ 
