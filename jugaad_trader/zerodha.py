c0947e1eceaa382b519837d93a1c1129dd7dc9a2
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2021-07-24 07:55:14 +0530

    Updated ticker method and removed ZerodhaTicker class

--- jugaad_trader/zerodha.py
+++ jugaad_trader/zerodha.py
@@ -25,24 +25,6 @@ from jugaad_trader.util import CLI_NAME
 
 log = logging.getLogger(__name__)
 
-class ZerodhaTicker(KiteTicker):
-    ROOT_URI = "wss://ws.zerodha.com/"
-    def __init__(self, user_id, enc_token, debug=False, 
-                                            root=None, reconnect=True,
-                                            reconnect_max_tries=KiteTicker.RECONNECT_MAX_TRIES,
-                                            reconnect_max_delay=KiteTicker.RECONNECT_MAX_DELAY,
-                                            connect_timeout=KiteTicker.CONNECT_TIMEOUT):
-        super(ZerodhaTicker, self).__init__(api_key="", access_token="",
-                                            debug=debug, root=root,
-                                            reconnect=reconnect,
-                                            reconnect_max_tries=reconnect_max_tries,
-                                            reconnect_max_delay=reconnect_max_delay,
-                                            connect_timeout=connect_timeout)
-
-        uid = int(time.time())*1000
-        self.socket_url = "{root}?api_key=kitefront&user_id={user_id}&enctoken={enc_token}&uid={uid}&user-agent=kite3-web&version=2.4.0".format(root=self.ROOT_URI, user_id=user_id, enc_token=enc_token, uid=uid)
-
-
 base_url = "https://kite.zerodha.com"
 login_url = "https://kite.zerodha.com/api/login"
 twofa_url = "https://kite.zerodha.com/api/twofa"
@@ -63,7 +45,7 @@ class Zerodha(KiteConnect):
     
         super().__init__(api_key="")
         self.s = self.reqsession = requests.Session()
-        headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+        headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
                     }
@@ -101,7 +83,7 @@ class Zerodha(KiteConnect):
         self.twofa = creds['twofa']
 
     def _user_agent(self):
-        return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+        return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"
     
     def login_step1(self):
         self.r = self.reqsession.get(base_url)
@@ -131,7 +113,7 @@ class Zerodha(KiteConnect):
         h = {}
         h['authorization'] = "enctoken {}".format(self.enc_token)
         h['referer'] = 'https://kite.zerodha.com/dashboard'
-        h['x-kite-version'] = '2.4.0'
+        h['x-kite-version'] = '2.9.2'
         h['sec-fetch-site'] = 'same-origin'
         h['sec-fetch-mode'] = 'cors'
         h['sec-fetch-dest'] = 'empty'
@@ -162,8 +144,8 @@ class Zerodha(KiteConnect):
             r = self.reqsession.request(method,
                                         url,
                                         json=params if (method in ["POST", "PUT"] and is_json) else None,
-                                        data=params if method in ["POST", "PUT"] else None,
-                                        params=query_params if method in ["GET", "DELETE"] else None,
+                                        data=params if (method in ["POST", "PUT"] and not is_json) else None,
+                                        params=query_params,
                                         headers=headers,
                                         verify=not self.disable_ssl,
                                         allow_redirects=True,
@@ -185,13 +167,13 @@ class Zerodha(KiteConnect):
                     content=r.content))
 
             # api error
-            if data.get("error_type"):
+            if data.get("status") == "error" or data.get("error_type"):
                 # Call session hook if its registered and TokenException is raised
                 if self.session_expiry_hook and r.status_code == 403 and data["error_type"] == "TokenException":
                     self.session_expiry_hook()
 
                 # native Kite errors
-                exp = getattr(ex, data["error_type"], ex.GeneralException)
+                exp = getattr(ex, data.get("error_type"), ex.GeneralException)
                 raise exp(data["message"], code=r.status_code)
 
             return data["data"]
@@ -208,7 +190,6 @@ class Zerodha(KiteConnect):
         bs = BeautifulSoup(html)
         for tag in bs.find_all("link"):
             src = tag.attrs.get("href", "")
-            # print(src)
             if "chunk" in src:
                 break
         url = urljoin(base_url, tag.attrs.get("href"))
@@ -233,8 +214,14 @@ class Zerodha(KiteConnect):
         self.reqsession.close()
    
     
-    def ticker(self):
-        return ZerodhaTicker(self.user_id, self.enc_token)
+    def ticker(self, api_key='kitefront', enctoken=None, userid=None):
+        if enctoken is not None:
+            self.enctoken = self.r.cookies['enc_token']
+        if userid is not None:
+            self.user_id = self.user_id
+        if self.user_id is None:
+            raise Exception("\nCould not find the session, Please start a session using \n\n$ jtrader zerodha startsession")
+        return KiteTicker(api_key=api_key, access_token=self.enc_token+'&user_id='+self.user_id, root='wss://ws.zerodha.com')
                             
 class Console(Zerodha):
     """
@@ -271,7 +258,7 @@ class Console(Zerodha):
 
         self.url_patch = ""
         self.reqsession = z.reqsession
-        headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+        headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
                     }
@@ -282,12 +269,12 @@ class Console(Zerodha):
     def custom_headers(self):
         h = {}
         h['referer'] = 'https://console.zerodha.com/'
-        h['x-kite-version'] = '2.4.0'
+        h['x-kite-version'] = '3'
         h['sec-fetch-site'] = 'same-origin'
         h['sec-fetch-mode'] = 'cors'
         h['sec-fetch-dest'] = 'empty'
-        h['user-agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
-        h['x-csrftoken'] = self.public_token
+        h['user-agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"
+
         return h
     
 
@@ -302,7 +289,6 @@ class Console(Zerodha):
         if self.r.url == 'https://console.zerodha.com/dashboard':
             cookies = self.reqsession.cookies.get_dict('console.zerodha.com')
             self.console_session = cookies['session']
-            self.public_token = self.reqsession.cookies['public_token']
             return True
         else:
             raise Exception("Login failed or Kite session expired")
