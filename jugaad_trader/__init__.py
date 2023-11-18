3ad912c8c36555a467a2df62fc0e480112b968a7
pablo escobar gaviria <abc@xyz.com>
2023-11-18 20:09:09 +0000

    CICD

--- jugaad_trader/__init__.py
+++ jugaad_trader/__init__.py
@@ -1,6 +1,6 @@
-
+from importlib.metadata import version
 from jugaad_trader import *
 from jugaad_trader.zerodha import Zerodha, Console
 from jugaad_trader.upstox import Upstox
 
-__version__ = "0.19"
+__version__ = version(__package__)
